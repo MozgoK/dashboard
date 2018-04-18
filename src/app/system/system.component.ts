e3326f833e9a76113a3ff0ec406440d627e1cdb6
Василий Кострикин <v.kostrikin@isimplelab.com>
2018-04-18 17:41:48 +0300

    Add dashboard template

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -5,5 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './system.component.html'
 })
 export class SystemComponent {
+  menuItems: any[] = [
+    { text: 'Главная', url: '/main', type: 'url', shape: 'dashboard', class: 'is-solid' },
+    { text: 'Защита', url: '/main', type: 'url', shape: 'shield', class: 'is-solid' },
+    { text: 'Блоки', url: '/main', type: 'url', shape: 'block', class: '' },
+    { text: 'Главная', url: '/main', type: 'heading' },
+    { text: 'Скорость', url: '/main', type: 'url', shape: 'bolt', class: 'is-solid' }
+  ];
 
+  // tslint:disable-next-line:no-inferrable-types
+  sidebarMini: boolean = false;
 }
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from '../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module';
 import { DesktopComponent } from './desktop/desktop.component';
 import { SystemComponent } from './system.component';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    SystemRoutingModule
+    SystemRoutingModule,
+    ClarityModule
   ],
   declarations: [
     SystemComponent,
